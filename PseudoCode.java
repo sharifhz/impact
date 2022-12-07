b1553c90bea0e56d10e81b6f063117778de35df6
CodeSubmit Team <CodeSubmit Team hello@info.codesubmit.io>
2022-12-07 19:52:59 +0000

    We wish you all the best! The CodeSubmit Team.

---
+++ PseudoCode.java
@@ -0,0 +1,18 @@
+/**
+ * Write the test cases for an endpoint that takes requests like this:
+ *   /convertLettersToNumbers?letters=aBc
+ * The endpoint takes in a series of letters in the "letters" query parameter
+ * and returns a numeric representation. For example, if the request has the
+ * query parameter "letters=a", the endpoint returns "01". If the request has
+ * "letters=aBz", the endpoint returns "010226".
+ * 
+ * Please identify and write the automated tests. 
+ * Do not implement the endpoint. Simply write the tests based on the specification of the endpoint.
+ *
+ * For example, do NOT write:
+ *  public static void main() { .. }
+ * 
+ * Instead, you will write:
+ *  void test_something() { .. }
+ *  void test_something_else() { .. }
+ */
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# tracking-qa-engineer-api-mrorwa
+* Write pseudo code (it doesn't have to compile) in the file PseudoCode.java.
+* Read the javadoc instructions in that java file carefully.
+* List your test cases as methods in there.
+* Write some pseudo code for how you would test the API.
 
+Click the **"SUBMIT CODE"** button in CodeSubmit when you are done
+to trigger our review process.
